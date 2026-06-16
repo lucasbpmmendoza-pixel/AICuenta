50123662bd1cf7fa334edaf610532126ada16caf
Sergio <sergiorm.mmendoza@gmail.com>
2026-06-16 12:19:31 -0600

    chikenelos hardcode retires

--- lib/rfc-aliases.ts
+++ lib/rfc-aliases.ts
@@ -1,7 +1,5 @@
 const RFC_ALIASES: Record<string, string> = {
   'BFR140708FJ9': 'Chikenelo_1',
-  'LIF101018K80': 'Chikenelo_2',
-  'LIF170822MZ3': 'Chikenelo_3',
 };
 
 export function rfcAlias(rfc: string): string | null {
